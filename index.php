9991ca7ee896170903b0f8f0511066cac2e86fc8
Rebekah <rebekah.kivett@gmail.com>
2021-02-01 22:31:09 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <?php include('./view/header.php'); ?>
+    <h1>
+    <?php
+        $firstname = filter_input(INPUT_GET, 'first', FILTER_SANITIZE_STRING);
+        $lastname = filter_input(INPUT_GET, 'last', FILTER_SANITIZE_STRING);
+
+        if (!empty($firstname) && !empty($lastname)) {
+            echo "Hello, my name is {$firstname} {$lastname}.";
+        } else {
+            echo "You did not submit firstname and lastname parameters.";
+        }
+    ?>
+    </h1>
+
+    <p>
+    <?php
+          $age = filter_input(INPUT_GET, 'age', FILTER_SANITIZE_STRING);
+
+          if (empty($age)) {
+              echo "You did not submit a numeric age parameter.";
+            } else if ($age >=18) {
+              echo "I am old enough to vote in the United States.";
+          } else if ($age < 18) {
+              echo "I am not old enough to vote in the United States.";
+          }
+    ?>
+    </p>
+
+    <p>
+    <?php
+        if (!empty($age)) {
+            echo "That means I'm at least " . $age * 365 . " days old.";
+        }
+    ?>
+    </p>
+
+
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,9 @@
+<header>
+
+<?php
+    $date = "02/01/2021";
+    
+    echo "Today is {$date}" ?>
+
+
+</header>
